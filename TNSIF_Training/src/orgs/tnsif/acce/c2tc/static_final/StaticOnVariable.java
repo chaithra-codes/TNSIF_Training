10a8812675f28788fd90e00c912f613fe3abd417
chaithra-codes <chaithrachaithra200512@gmail.com>
2026-08-07 12:34:47 +0530

    first commit

---
+++ TNSIF_Training/src/orgs/tnsif/acce/c2tc/static_final/StaticOnVariable.java
@@ -0,0 +1,31 @@
+package orgs.tnsif.acce.c2tc.static_final;
+
+class Student
+{
+	int id;
+	String name;
+	static String collegename="Cambridge";
+	
+	
+	Student(int i ,String n)
+	{
+		id=i;
+		name=n;
+	}
+	void display() {
+		System.out.println("Id : "+ id +" " +"Name :"+name+" " +"CollegeName : "+collegename);
+	}
+}
+public class StaticOnVariable {
+
+	public static void main(String[] args) {
+		Student student=new Student(12,"Sushma");
+		Student student1=new Student(13,"Anil");
+		Student.collegename="CITNC";
+        student.display();
+        student1.display();
+		// TODO Auto-generated method stub
+
+	}
+
+}
