04eec08b9ef237cf65aebc49b0e939511975542e
chaithra-codes <chaithrachaithra200512@gmail.com>
2026-08-09 21:14:13 +0530

    first commit

---
+++ TNSIF_Training/src/org/tnsif/acce/c2tc/interview_preparation/FibonacciSeries.java
@@ -0,0 +1,26 @@
+package org.tnsif.acce.c2tc.interview_preparation;
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		 Scanner scanner = new Scanner(System.in);
+         System.out.print("Enter a number: ");
+         int count = scanner.nextInt();
+         int num1 = 0, num2 = 1;
+
+         System.out.print("Fibonacci Series of " + count + " numbers: ");
+
+         for (int i = 1; i <= count; ++i) {
+             System.out.print(num1 + " ");
+             int sum = num1 + num2;
+             num1 = num2;
+     
+
+   
+	 num2 = sum;
+  }
+ }
+ }
+
+
