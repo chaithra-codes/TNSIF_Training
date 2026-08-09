582145a20c34bc855c75bf23803bb7c11a22d68a
chaithra-codes <chaithrachaithra200512@gmail.com>
2026-08-09 21:17:03 +0530

    first commit

---
+++ TNSIF_Training/src/org/tnsif/acce/c2tc/interview_preparation/ReverseString.java
@@ -0,0 +1,23 @@
+package org.tnsif.acce.c2tc.interview_preparation;
+import java.util.Scanner;
+
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = scanner.nextLine();
+        String reversedStr = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversedStr += str.charAt(i);
+        }
+
+        System.out.println("Reversed string: " + reversedStr);
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
