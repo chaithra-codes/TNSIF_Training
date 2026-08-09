8bf0f02f4fd3fd5232293e0c013db193fa7e5173
chaithra-codes <chaithrachaithra200512@gmail.com>
2026-08-09 21:23:08 +0530

    first commit

---
+++ TNSIF_Training/src/org/tnsif/acce/c2tc/interview_preparation/Palindrome.java
@@ -0,0 +1,28 @@
+package org.tnsif.acce.c2tc.interview_preparation;
+import java.util.Scanner;
+
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = scanner.nextLine();
+        String reversedStr = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversedStr += str.charAt(i);
+        }
+
+        if (str.equals(reversedStr)) {
+            System.out.println(str + " is a palindrome.");
+        } else {
+            System.out.println(str + " is not a palindrome.");
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
+	
+}
