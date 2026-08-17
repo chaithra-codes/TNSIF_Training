434eede4485796eea2b3fde011b1b2eab0037c89
chaithra-codes <chaithrachaithra200512@gmail.com>
2026-08-17 19:37:04 +0530

    first commit

---
+++ TNSIF_Training/src/org/tnsif/acce/c2tc/interview_preparation/SimpleSumArray.java
@@ -0,0 +1,24 @@
+package org.tnsif.acce.c2tc.interview_preparation;
+
+public class SimpleSumArray {
+
+	public static void main(String[] args) {
+		 //Initialize an array of integers 
+
+		 int[] array = {2, 4, 6, 8, 10}; 
+
+		 // Calculate the sum of elements in the array 
+
+		 int sum = 0; for (int num : array) { sum += num; 
+		 } 
+		 // Print the sum of elements 
+
+		 System.out.println("Sum of elements in the array: " + sum); 
+		     } 
+		 }
+
+
+
+	
+
+
