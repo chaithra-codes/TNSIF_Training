d0d3c3fa030ec9f6bc38a943d498362b3907759d
chaithra-codes <chaithrachaithra200512@gmail.com>
2026-08-17 19:14:20 +0530

    first commiit

---
+++ TNSIF_Training/src/org/tnsif/acce/c2tc/interview_preparation/SumOfNaturalNumbers.java
@@ -0,0 +1,31 @@
+package org.tnsif.acce.c2tc.interview_preparation;
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers {
+	
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		 // Prompt the user to enter a number 
+		System.out.print("Enter a number: "); 
+		int n = scanner.nextInt(); 
+
+		// Calculate the sum of natural numbers up to n
+
+		 int sum = 0; for (int i = 1; i <= n; i++)
+		 { 
+		sum += i;
+		 } 
+
+		// Print the sum 
+		System.out.println("Sum of natural numbers up to " + n + " is: " + sum); 
+
+		// Close the scanner 
+		scanner.close();
+		   } 
+
+		
+	}
+
+
