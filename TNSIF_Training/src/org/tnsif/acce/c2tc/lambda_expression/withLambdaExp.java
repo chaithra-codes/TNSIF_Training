e68371d3bc637dde727a87d4b5160866bb39f7e4
chaithra-codes <chaithrachaithra200512@gmail.com>
2026-08-27 18:11:06 +0530

    first commit

---
+++ TNSIF_Training/src/org/tnsif/acce/c2tc/lambda_expression/withLambdaExp.java
@@ -0,0 +1,19 @@
+package org.tnsif.acce.c2tc.lambda_expression;
+@FunctionalInterface    
+interface Drawable{  
+    public void draw();  
+}  
+
+public class withLambdaExp {
+
+	public static void main(String[] args) {
+		int width=10;    
+        //with lambda  
+        Drawable d2=()->{  
+            System.out.println("Drawing "+width);  
+        };  
+        d2.draw();  
+    }  
+}  
+
+	
