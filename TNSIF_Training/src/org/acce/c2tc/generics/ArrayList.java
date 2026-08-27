6a17c6c043890bc9417a8a0238b35cd4ba072675
chaithra-codes <chaithrachaithra200512@gmail.com>
2026-08-27 18:31:04 +0530

    first commit

---
+++ TNSIF_Training/src/org/acce/c2tc/generics/ArrayList.java
@@ -0,0 +1,17 @@
+package org.acce.c2tc.generics;
+import java.util.List;
+
+public class ArrayList {
+
+	public static void main(String[] args) {
+		List list=(List) new ArrayList();
+		list.add("abc");
+		list.add(new Integer(5));
+		for(Object obj : list){
+			//type casting leading to ClassCastException at runtime
+		    String str=(String) obj; 
+		}
+
+	}
+
+}
